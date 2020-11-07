99f0d5a4cbfc256b18ec7bb6758dbcfe19dc7a5b
JanardhanSingh98 <janardhansingh1998@gmail.com>
2020-11-07 14:17:14 +0530

    added created and updated at field in json

--- app/serializers.py
+++ app/serializers.py
@@ -9,10 +9,12 @@ class AuthorSerializers(serializers.ModelSerializer):
     f_n = serializers.CharField(source='first_name')
     l_n = serializers.CharField(source='last_name')
     nt = serializers.CharField(source='notes', required=False)
+    cr_at = serializers.ReadOnlyField(source='created_at', required=False)
+    up_at= serializers.ReadOnlyField(source='updated_at', required=False)
 
     class Meta:
         model = Author
-        fields = ['id', 'au_id', 'f_n', 'l_n', 'email', 'nt']
+        fields = ['id', 'au_id', 'f_n', 'l_n', 'email', 'nt', 'cr_at', 'up_at']
 
 
 class BookSerializers(serializers.ModelSerializer):
@@ -20,7 +22,9 @@ class BookSerializers(serializers.ModelSerializer):
     ti = serializers.CharField(source='title')
     desc = serializers.CharField(source='description')
     au_id = serializers.PrimaryKeyRelatedField(source='author', read_only=False, queryset=Author.objects.all())
+    cr_at = serializers.ReadOnlyField(source='created_at', required=False)
+    up_at= serializers.ReadOnlyField(source='updated_at', required=False)
 
     class Meta:
         model = Book
-        fields = ['bo_id', 'ti', 'desc', 'au_id']
+        fields = ['bo_id', 'ti', 'desc', 'au_id', 'cr_at', 'up_at']
